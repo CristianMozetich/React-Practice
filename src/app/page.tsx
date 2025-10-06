bef3ed5db99b958c88b49f237a716c50a71d1f2e
Cristian Mozetich <cristianmozetich@gmail.com>
2025-10-06 20:38:25 +0100

    3 commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,22 @@
+"use client";
 import { Movies } from "../components/Movies";
 import { useMovie } from "../hooks/useMovies";
-
-
+import { useRef } from "react";
 
 export default function Home() {
+  const inputRef = useRef<HTMLInputElement>({} as HTMLInputElement);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(inputRef.current.value);
+  }
+
   const { movies: mappedMovies } = useMovie();
   return (
     <div className="flex flex-col items-center bg-cyan-950 text-amber-50 justify-center">
       <form className="flex" action="">
-        <input className="m-2" type="text" placeholder="Busca tu pelicula" />
-        <button className="m-2" type="submit">
+        <input ref={inputRef} className="m-2" type="text" placeholder="Busca tu pelicula" />
+        <button onClick={handleSubmit} className="m-2" type="submit">
           Buscar
         </button>
       </form>
